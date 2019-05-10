eaf2ee18d2aa206a06157bef53342fb512ca498b
sattiti <sattiti@users.noreply.github.com>
2019-05-10 12:47:59 +0900

    update

--- src/displayElsAttrs.js
+++ src/displayElsAttrs.js
@@ -1,21 +1,26 @@
 (function(w,d){
-  var nodes = d.querySelectorAll('*');
+  function assertError(){
+    alert('No such Selectors or Elements.');
+    return false;
+  };
 
   function randomHexColor(){
-    var v = '#';
-    var c = (()=>{
-      var a = Math.round(Math.ceil(Math.random() * parseInt(0xff))).toString(16);
+    let v   = '#';
+    const c = (()=>{
+      let a = Math.round(Math.ceil(Math.random() * parseInt(0xff))).toString(16);
       return a.length !== 2 ? '0' + a : a;
     });
-    for(var i=0;i<3;i++){
+    for(var i=0; i<3; i++){
       v += c();
     }
     return v;
   };
 
-  function getAttrAll(n){
-    var attr = '';
-    for(var name of n.getAttributeNames()){
+  function getAttrs(n){
+    var attr        = '';
+    const attrNames = n.getAttributeNames();
+
+    for(var name of attrNames){
       if(name === 'id'){
         attr = attr + "#" + n.getAttribute(name);
       }
@@ -23,48 +28,70 @@
         attr = attr + '.' + n.getAttribute(name);
       }
       else{
-        attr = attr + '[' + name+'="' + n.getAttribute(name) + '"]';
+        attr = (attr + '[' + name + '="' + n.getAttribute(name) + '"]').replace('][', ' ');
       }
     };
     return attr;
   };
 
-  var nodesSize = nodes.length;
-  for(var i=0;i<nodesSize;i++){
-    var n   = nodes[i];
-    var tag = n.tagName.toLowerCase();
-    var t   = tag;
-    if(!tag.match(/script|link|noscript|head|meta|style|br/img)){
-      var label = d.createElement('span');
-      label.setAttribute('class', 'MYLabel');
-      label.style.color           = 'yellow';
-      label.style.backgroundColor = 'purple';
-      label.style.position        = 'relative';
-      label.style.display         = 'block';
-      label.style.padding         = '5px 10px';
-      label.style.left            = 0;
-      label.style.top             = 0;
-      label.style.fontSize        = '14px';
-      label.style.border          = '1px solid black';
-      label.style.height          = 'auto';
-      label.style.width           = 'auto';
-      label.style.clear           = 'both';
-      label.style.weight          = 'normal';
-      label.style.textWrap        = 'nowrap';
-      label.style.wordBreak       = 'break-word';
-      label.style.whiteSpace      = 'pre-wrap';
-      label.style.textAlign       = 'left';
-      label.style.verticalAlign   = 'top';
-      label.textContent           = t + getAttrAll(n);
+  let io = w.prompt('Input a Selector or tagName:', '*');
+  if(io.length <= 0 || io === null || io == undefined){
+    assertError();
+    return false;
+  };
 
-      if(tag === 'img' || tag === 'br'){
-        n.parentNode.insertBefore(label, n.parentNode.childNodes[0]||null);
-      }
-      else{
-        n.insertBefore(label, n.childNodes[0]||null);
-      }
-      n.style.border = '2px solid ' + randomHexColor();
-      n.style.margin = '5px';
-    }
+  try{
+    const els     = d.querySelectorAll(io);
+    const elsSize = els.length;
+
+    for(var i=0;i< elsSize;i++){
+      const o   = els[i];
+      const tag = o.tagName.toLowerCase();
+
+      if(!tag.match(/script|link|noscript|head|meta|style|br/img)){
+        let c = d.createElement('span');
+        c.setAttribute('class', 'MYLabel');
+        c.style.display         = 'block';
+        c.style.position        = 'relative';
+        c.style.left            = 0;
+        c.style.top             = 0;
+        c.style.zIndex          = 2147483647;
+        c.style.width           = 'auto';
+        c.style.height          = 'auto';
+        c.style.backgroundColor = 'purple';
+        c.style.color           = 'yellow';
+        c.style.padding         = '4px 8px';
+        c.style.fontSize        = '14px';
+        c.style.height          = 'auto';
+        c.style.width           = 'auto';
+        c.style.lineHeight      = 1.5;
+        c.style.clear           = 'both';
+        c.style.fontWeight      = 'normal';
+        c.style.wordBreak       = 'break-all';
+        c.style.whiteSpace      = 'pre-wrap';
+        c.style.textAlign       = 'left';
+        c.style.verticalAlign   = 'top';
+        c.textContent           = tag + getAttrs(o);
+
+        if(o.parentNode && o.parentNode.style){
+          o.parentNode.style.position = 'relative';
+        };
+
+        if(tag === 'img' || tag === 'br'){
+          o.parentNode.insertBefore(c, o.parentNode.childNodes[0] || null);
+        }
+        else{
+          o.insertBefore(c, o.childNodes[0] || null);
+        };
+
+        o.style.border     = '2px solid ' + randomHexColor();
+        o.style.margin     = '5px';
+        o.style.fontWeight = 'normal';
+      };
+    };
+  }
+  catch(err){
+    console.log(err);
+    assertError();
   }
 })(window,document);
