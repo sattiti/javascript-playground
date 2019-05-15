919b75dd8d49895da90afb4b3672d5eba075d256
sattiti <sattiti@users.noreply.github.com>
2019-05-15 22:53:37 +0900

    add

---
+++ src/jqueryplugin.template.js
@@ -0,0 +1,11 @@
+;(function($){
+  $.fn.PLUGIN_NAME = function(options){
+    var opts = $.extend({
+    
+    }, options);
+    
+    return this.each(function(){
+      
+    });
+  }
+})(jQuery);
