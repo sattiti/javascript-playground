c4218025b92e49c850bdc170e9ae16e6366b89f7
sattiti <24436239+sattiti@users.noreply.github.com>
2019-07-17 17:25:19 +0900

    add

---
+++ src/vue/mixins/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>{{hello}}</p>
+</template>
+
+
+<script>
+import Hello from './hello'
+
+export default {
+  mixins: [Hello],
+  data: function(){
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+p{
+  font-weight:bold;
+  margin-left:auto;
+  margin-right:auto;
+}
+</style>
