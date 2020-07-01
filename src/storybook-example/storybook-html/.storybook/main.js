cfca797dbeab4da4b3a76b6f208d12a70c786e3e
sattiti <24436239+sattiti@users.noreply.github.com>
2020-07-01 11:29:22 +0900

    update

--- src/storybook-example/storybook-html/.storybook/main.js
+++ src/storybook-example/storybook-html/.storybook/main.js
@@ -1,1 +1,71 @@
+const path = require('path')
 
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.[tj]s'
+  ],
+
+  addons: [
+    '@whitespace/storybook-addon-html/register',
+    '@storybook/addon-a11y',
+    // '@storybook/addon-actions',
+    // '@storybook/addon-backgrounds',
+    // '@storybook/addon-console/register',
+    // '@storybook/addon-design-assets',
+    // '@storybook/addon-docs',
+    // '@storybook/addon-events/register',
+    // '@storybook/addon-knobs',
+    // '@storybook/addon-links/register',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport',
+  ],
+
+  webpackFinal: async (config, { configType })=>{
+    config.module.rules.push(
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // implementation: require('sass'),
+              sassOptions: {
+                // fiber: require('fibers'),
+                indentedSyntax: true,
+                outputStyle: 'compressed',
+                sourceMap: false,
+              },
+            },
+          }
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'html-loader'
+          },
+          {
+            loader: 'pug-plain-loader',
+            options: {
+              pretty: true,
+            }
+          },
+        ],
+      },
+    )
+
+    config.plugins.push()
+
+    return config
+  },
+};
