693899d095760f578b44ceb4a7ef07656848ee28
sattiti <sattiti@users.noreply.github.com>
2020-07-02 08:20:20 +0900

    update

--- src/storybook-example/storybook-html/.storybook/preview.js
+++ src/storybook-example/storybook-html/.storybook/preview.js
@@ -1,5 +1,15 @@
-import { addDecorator } from '@storybook/html'
+// preview.js の内容は全ての story に適応される。
+
+// Decorators
+// A decorator is a way to wrap a story with some formatting or some common set of components.
+
+// Parameters
+// Parameters are custom metadata for a story. 
+
+
+import { addDecorator, addParameters } from '@storybook/html'
 import { withHTML } from '@whitespace/storybook-addon-html/html'
+
 
 addDecorator(
   withHTML({
