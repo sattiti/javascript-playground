319f18bf2b8fcf7db283394450ee78765156b8b9
sattiti <24436239+sattiti@users.noreply.github.com>
2020-07-01 11:30:13 +0900

    add

---
+++ src/storybook-example/storybook-html/.storybook/manager.js
@@ -0,0 +1,29 @@
+import { addons } from '@storybook/addons'
+import { themes } from '@storybook/theming'
+import myTheme from './theme'
+
+const options = {
+  isFullScreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'bottom',
+  sidebarAnimations: true,
+  enableShortcuts: true,
+  isToolshown: true,
+  /**
+   * id to select an addon panel
+   * @type {String}
+   */
+  selectedPanel: undefined,
+}
+
+
+addons.setConfig({
+  ...options,
+
+  // Preset theme
+  // theme: themes.dark,
+
+  // Own theme
+  theme: myTheme,
+})
