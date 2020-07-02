91e33c24c1a8644cf52307509b1120c84809b813
sattiti <24436239+sattiti@users.noreply.github.com>
2020-07-02 17:11:37 +0900

    update

--- src/storybook-example/storybook-html/.storybook/main.js
+++ src/storybook-example/storybook-html/.storybook/main.js
@@ -2,22 +2,30 @@ const path = require('path')
 
 module.exports = {
   stories: [
-    '../stories/**/*.stories.[tj]s'
+    '../stories/**/*.stories.@(ts|js|jsx|mdx)',
+    // '../src/**/*.stories.@(ts|js|mdx)',
   ],
 
   addons: [
     '@whitespace/storybook-addon-html/register',
     '@storybook/addon-a11y',
-    // '@storybook/addon-actions',
+    '@storybook/addon-actions',
     // '@storybook/addon-backgrounds',
     // '@storybook/addon-console/register',
     // '@storybook/addon-design-assets',
-    // '@storybook/addon-docs',
     // '@storybook/addon-events/register',
     // '@storybook/addon-knobs',
     // '@storybook/addon-links/register',
     '@storybook/addon-storysource',
     '@storybook/addon-viewport',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      },
+    },
   ],
 
   webpackFinal: async (config, { configType })=>{
@@ -64,7 +72,7 @@ module.exports = {
       },
     )
 
-    config.plugins.push()
+    // config.plugins.push()
 
     return config
   },
